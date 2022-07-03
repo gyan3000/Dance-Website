40ffccada11b23c0dae924cacd255be15b24cde9
Gyaneshwar <gyaneshwar.knp.2019@gmail.com>
2022-07-03 22:42:47 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+const port = 800
+
+//express specific stuff
+app.use('/static', express.static('static'))//For serving static files
+app.use(express.urlencoded())
+
+// pug specific stuff
+app.set('view engine', 'pug')  // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views'))
+
+
+//end points
+app.get('/',(req,res)=>{
+    
+    const param = { };
+    res.status(200).render('index.pug',param);
+})
+
+app.get('/contacts',(req,res)=>{
+    
+    const param = { };
+    res.status(200).render('contact.pug',param);
+})
+
+
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log("The application Started");
+})
